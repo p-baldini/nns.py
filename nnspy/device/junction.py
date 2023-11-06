d5ddf71f274d7ef71fb16dd0df5395af9b1b565a
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-06 16:17:49 +0100

    added function for junctions comparison

--- nnspy/device/junction.py
+++ nnspy/device/junction.py
@@ -1,5 +1,7 @@
-from ctypes import c_int, Structure
+from ctypes import c_int, c_void_p, CDLL, Structure
 from nnspy.util.point import point
+
+nns = CDLL("libnns.so")
 
 class junction(Structure):
     _fields_ = [
@@ -7,3 +9,6 @@ class junction(Structure):
         ("second_wire", c_int),
         ("position",    point)
     ]
+
+nns.jcmp.argtypes = c_void_p, c_void_p
+nns.jcmp.restype = c_int
