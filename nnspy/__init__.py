abf4937c91b4a6274c918833e285834eefdca830
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-06 16:43:57 +0100

    removed functions initialization from __init__.py

--- nnspy/__init__.py
+++ nnspy/__init__.py
@@ -1,4 +1,5 @@
 from ctypes import c_double, c_int, CDLL, POINTER
+
 from nnspy.device.component import connected_component
 from nnspy.device.datasheet import datasheet
 from nnspy.device.junction import junction
@@ -9,24 +10,6 @@ from nnspy.interface.mea import MEA
 from nnspy.util.point import point
 
 nns = CDLL("libnns.so")
-
-nns.create_network.argtypes = datasheet, POINTER(c_int), POINTER(c_int)
-nns.create_network.restype = network_topology
-
-nns.construe_circuit.argtypes = datasheet, network_topology
-nns.construe_circuit.restype = network_state
-
-nns.split_components.argtypes = datasheet, network_topology, POINTER(c_int), c_int
-nns.split_components.restype = POINTER(connected_component)
-
-nns.voltage_stimulation.argtypes = network_state, connected_component, interface, POINTER(c_double)
-nns.voltage_stimulation.restype = c_int
-
-nns.resistive_distance.argtypes = network_state, connected_component, c_int, c_int
-nns.resistive_distance.restype = c_double
-
-nns.connect_MEA.argtypes = datasheet, network_topology
-nns.connect_MEA.restype = MEA
 
 __all__ = [
 
