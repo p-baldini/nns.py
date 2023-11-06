ad56d4b7a82a000f381babb6fade826078b2f0b5
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-06 15:15:16 +0100

    corrected mea structure after changes in C library

--- nnspy/interface/mea.py
+++ nnspy/interface/mea.py
@@ -1,8 +1,10 @@
-from ctypes import c_int, Structure
+from ctypes import c_double, c_int, Structure
 from nnspy.util.point import point
 
 class MEA(Structure):
     _fields_ = [
         ("Ps",      point * 16),
-        ("e2n",     c_int * 16)
+        ("e2n",     c_int * 16),
+        ("ct",      c_int * 16),
+        ("ws",      c_double * 16)
     ]
