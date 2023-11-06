6fb683cc86a3657bceafe595090dddb0be00b1f9
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-06 16:28:15 +0100

    added point distance calculation function

--- nnspy/util/point.py
+++ nnspy/util/point.py
@@ -1,7 +1,12 @@
-from ctypes import c_double, Structure
+from ctypes import c_double, CDLL, Structure
+
+nns = CDLL("libnns.so")
 
 class point(Structure):
     _fields_ = [
         ("x", c_double),
         ("y", c_double)
     ]
+
+nns.squared_distance.argtypes = point, point
+nns.squared_distance.restype = c_double
