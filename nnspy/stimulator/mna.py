76a8f42a2128a0d3b933403c83644467f5f8fdfa
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-08 15:16:35 +0100

    specified exported files in stimulators files and corrected imports

--- nnspy/stimulator/mna.py
+++ nnspy/stimulator/mna.py
@@ -1,13 +1,14 @@
-from ctypes import c_double, c_int, CDLL, POINTER
+from ctypes import c_double, c_int, POINTER
 from nnspy.device.network import network_state
 from nnspy.device.component import connected_component
 from nnspy.interface.interface import interface
-from nnspy.interface.MEA import MEA
-
-nns = CDLL("libnns.so")
+from nnspy.interface.mea import MEA
+from nnspy.nns import nns
 
 nns.voltage_stimulation.argtypes = network_state, connected_component, interface, POINTER(c_double)
 nns.voltage_stimulation.restype = c_int
 
 nns.voltage_stimulation_mea.argtypes = network_state, connected_component, MEA, POINTER(c_double)
 nns.voltage_stimulation_mea.restype = c_int
+
+__all__ = "nns",
--- nnspy/stimulator/update.py
+++ nnspy/stimulator/update.py
@@ -1,7 +1,7 @@
-from ctypes import CDLL
 from nnspy.device.network import network_state
 from nnspy.device.component import connected_component
-
-nns = CDLL("libnns.so")
+from nnspy.nns import nns
 
 nns.update_conductance.argtypes = network_state, connected_component
+
+__all__ = "nns",
