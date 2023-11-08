2374b7a3efb656a5a3d9899a135a169e610a8f4b
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-08 15:15:50 +0100

    specified exported files in io files and corrected imports

--- nnspy/io/deserializer.py
+++ nnspy/io/deserializer.py
@@ -1,18 +1,19 @@
-from ctypes import c_char, c_int, CDLL, POINTER
+from ctypes import c_char_p, c_int, POINTER
 from nnspy.device.component import connected_component
 from nnspy.device.datasheet import datasheet
 from nnspy.device.network import network_topology, network_state
 from nnspy.interface.interface import interface
 from nnspy.interface.mea import MEA
+from nnspy.nns import nns
 
-nns = CDLL("libnns.so")
+nns.deserialize_network.argtypes = POINTER(datasheet), POINTER(network_topology), c_char_p
 
-nns.deserialize_network.argtypes = POINTER(datasheet), POINTER(network_topology), POINTER(c_char), c_int
+nns.deserialize_state.argtypes = datasheet, network_topology, POINTER(network_state), c_char_p, c_int, c_int
 
-nns.deserialize_state.argtypes = datasheet, network_topology, POINTER(network_state), POINTER(c_char), c_int, c_int
+nns.deserialize_component.argtypes = POINTER(connected_component), c_char_p, c_int, c_int
 
-nns.deserialize_component.argtypes = POINTER(connected_component), POINTER(c_char), c_int, c_int
+nns.deserialize_interface.argtypes = POINTER(interface), c_char_p, c_int, c_int
 
-nns.deserialize_interface.argtypes = POINTER(interface), POINTER(c_char), c_int, c_int
+nns.deserialize_mea.argtypes = POINTER(MEA), c_char_p, c_int, c_int
 
-nns.deserialize_mea.argtypes = POINTER(MEA), POINTER(c_char), c_int, c_int
+__all__ = "nns",
--- nnspy/io/serializer.py
+++ nnspy/io/serializer.py
@@ -1,18 +1,19 @@
-from ctypes import c_char, c_int, CDLL, POINTER
+from ctypes import c_char_p, c_int, POINTER
 from nnspy.device.component import connected_component
 from nnspy.device.datasheet import datasheet
 from nnspy.device.network import network_topology, network_state
 from nnspy.interface.interface import interface
 from nnspy.interface.mea import MEA
+from nnspy.nns import nns
 
-nns = CDLL("libnns.so")
+nns.serialize_network.argtypes = datasheet, network_topology, c_char_p, c_int
 
-nns.serialize_network.argtypes = datasheet, network_topology, POINTER(c_char), c_int
+nns.serialize_state.argtypes = datasheet, network_topology, network_state, c_char_p, c_int, c_int
 
-nns.serialize_state.argtypes = datasheet, network_topology, network_state, POINTER(c_char), c_int, c_int
+nns.serialize_component.argtypes = connected_component, c_char_p, c_int, c_int
 
-nns.serialize_component.argtypes = connected_component, POINTER(c_char), c_int, c_int
+nns.serialize_interface.argtypes = interface, c_char_p, c_int
 
-nns.serialize_interface.argtypes = interface, POINTER(c_char), c_int, c_int
+nns.serialize_mea.argtypes = MEA, c_char_p, c_int, c_int
 
-nns.serialize_mea.argtypes = MEA, POINTER(c_char), c_int, c_int
+__all__ = "nns",
